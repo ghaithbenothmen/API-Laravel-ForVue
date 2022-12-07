f6c3c710a79ce2563b0edce4dc3192a0755d1bd4
Ben Othmen Ghaith <ghaithbenothmen8@gmail.com>
2022-12-08 00:43:06 +0100

    initial commit

---
+++ app/Http/Controllers/Api/ProfilController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Profil;
+use Illuminate\Http\Request;
+
+class ProfilController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Profil  $profil
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Profil $profil)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Profil  $profil
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Profil $profil)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Profil  $profil
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Profil $profil)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ProfilController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profil;
+use Illuminate\Http\Request;
+
+class ProfilController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Profil::select('id','nom','email')->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // !!!!!!! in front end VUEJS
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nom'=>'required',
+            'email'=>'required',
+        ]);
+        Profil::create($request->post());
+        return response()->json([
+            'message'=>'profil bien cree'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Profil  $profil
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Profil $profil)
+    {
+        return response()->json([
+            'profil'=>$profil
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Profil  $profil
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Profil $profil)
+    {
+        //VUEjs!! in ffront end !!!!!!
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Profil  $profil
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $profil=Profil::findOrFail($id);
+        $request->validate([
+            'nom'=>'required',
+            'email'=>'required',
+        ]);
+        $profil->fill($request->post())->update();
+        return response()->json([
+            'message'=>'profil bien a jour'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Profil  $profil
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Profil $profil)
+    {
+        $profil->delete();
+        return response()->json([
+            'message'=>'profil bien supprimer'
+        ]);
+    }
+}
